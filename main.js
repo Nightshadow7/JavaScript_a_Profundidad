97fc92a4b5a71c9df3502bc08d4a03f5ed3e468f
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-11-17 16:04:03 -0500

    feat :sparkles: implementation of the navbar in mobile views on the web page using Javascript

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const logOut = document.querySelector(".navbar-email");
 const menu = document.querySelector(".desktop-menu");
+const iconmobile= document.querySelector(".menu");
+const navmobile = document.querySelector(".mobile-menu");
 
 logOut.addEventListener("click", toggleLogOut);
+iconmobile.addEventListener("click", toggleNavMobile);
 
 function toggleLogOut() {
   menu.classList.toggle("inactive");
+}
+
+function toggleNavMobile() {
+  navmobile.classList.toggle("inactive");
 }
